12e8559e03f6c59efabdb41b65ed3fe0a37dc5b0
hat-7777777 <57338967+hat-7777777@users.noreply.github.com>
2020-01-16 11:43:45 -0800

    Fixed Mail system

--- tempalertTest.py
+++ tempalertTest.py
@@ -6,19 +6,18 @@ from email.mime.multipart import MIMEMultipart
 global email
 global password
 global send_to_email
-global msg
 
 email = "pythonmailtest7777777"
 password = "123!@#qwe"
 
 send_to_email = input("What email should receive temperature reports? ")
 
-msg = MIMEMultipart()
-msg["From"] = email
-msg["To"] = send_to_email
-msg["Subject"] = "Temperature Report"
-
 def main():
+    msg = MIMEMultipart()
+    msg["From"] = email
+    msg["To"] = send_to_email
+    msg["Subject"] = "Temperature Report"
+    
     temperature = round(random.uniform(40.0, 80.0), 1)
     time = datetime.now().strftime('%H:%M:%S %d-%m-%Y')
     msg.attach(MIMEText(f"The temperature is {temperature}°C at {time}.", "plain"))
@@ -28,8 +27,10 @@ def main():
     server.login(email, password)
     text = msg.as_string()
     server.sendmail(email, send_to_email, text)
+    print(f"Sent email at {time}")
+    msg = None
     server.quit()
 
 while True:
     main()
-    time.sleep(60)
+    time.sleep(10)
