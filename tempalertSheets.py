e137496c39bec16653e20c6befa6083e0b9ac0cb
hat-7777777 <thebuilder190@gmail.com>
2020-02-10 11:53:22 -0800

    Fills in data, leaves misreads as "-"

--- tempalertSheets.py
+++ tempalertSheets.py
@@ -1,17 +1,30 @@
-import gspread, pprint, random, time
+import gspread, random, time, board, adafruit_dht
 from oauth2client.service_account import ServiceAccountCredentials
 
+global dhtDevice
+
+index = 1
 scope = ["https://www.googleapis.com/auth/drive"]
 creds = ServiceAccountCredentials.from_json_keyfile_name("client_secret.json", scope)
 client = gspread.authorize(creds)
 
 sheet = client.open("Temperature Data Thing 3: The Party").sheet1
-index = 0
-temp = ["Yes", "No"]
+dhtDevice = adafruit_dht.DHT11(board.D17)
+
+def main():
+    global index
+    bruh = "No data"
+    try:
+        temperature_c = dhtDevice.temperature
+
+        sheet.update_cell(index, 1, temperature_c)
+    except RuntimeError as error:
+        print(error.args[0])
+        sheet.update_cell(index, 1, "-")
+
+    index += 1
+    print("index =", index)
 
 while True:
-    num = random.randint(0,100)
-    index += 1
-    
-    sheet.update_cell(index, 1, num)
-    time.sleep(1)
+    main()
+    time.sleep(10)
